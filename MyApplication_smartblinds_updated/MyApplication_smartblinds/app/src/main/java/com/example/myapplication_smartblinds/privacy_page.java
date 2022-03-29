a76d97bd92bd5f36869a98066f31e091d0dad229
shanjayk95 <shanjay.kailayanathan@ontariotechu.net>
2022-03-29 13:38:11 -0400

    Working privacy page.

--- MyApplication_smartblinds_updated/MyApplication_smartblinds/app/src/main/java/com/example/myapplication_smartblinds/privacy_page.java
+++ MyApplication_smartblinds_updated/MyApplication_smartblinds/app/src/main/java/com/example/myapplication_smartblinds/privacy_page.java
@@ -7,10 +7,19 @@ import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class privacy_page extends AppCompatActivity {
-ToggleButton tb;
-TextView tx3,tx4;
+
+    MemoryPersistence persistence = new MemoryPersistence();
+
+    ToggleButton tb;
+    TextView tx3,tx4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +34,7 @@ TextView tx3,tx4;
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
                 if(tb.isChecked()){
+                    blindsDown();
                     tx3.setText("IS ON");
                     tx3.setVisibility(View.VISIBLE);
                 }
@@ -36,5 +46,37 @@ TextView tx3,tx4;
         });
 
 
+    }
+
+    public void blindsDown (){
+        String topic        = "BlindTest";
+        String content      = "CLOSE";
+        int qos             = 2;
+        String broker       = "tcp://broker.hivemq.com:1883";
+        String clientId     = "SmartBlindsApp";
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(false);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            //System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
     }
 }
