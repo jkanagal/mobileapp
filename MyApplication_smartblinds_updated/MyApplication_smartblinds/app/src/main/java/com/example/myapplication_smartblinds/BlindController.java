7dc6a6815c30da5093a9663b6f2026bb173a6265
shanjayk95 <shanjay.kailayanathan@ontariotechu.net>
2022-03-30 17:44:14 -0400

    Finished schedule page.

---
+++ MyApplication_smartblinds_updated/MyApplication_smartblinds/app/src/main/java/com/example/myapplication_smartblinds/BlindController.java
@@ -0,0 +1,78 @@
+package com.example.myapplication_smartblinds;
+
+import android.view.View;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+
+public class BlindController {
+
+    MemoryPersistence persistence = new MemoryPersistence();
+
+    public void blindsUp (){
+        String topic        = "BlindTest";
+        String content      = "OPEN";
+        int qos             = 2;
+        String broker       = "tcp://broker.hivemq.com:1883";
+        String clientId     = "SmartBlindsApp";
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(false);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            //System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+
+    public void blindsDown (){
+        String topic        = "BlindTest";
+        String content      = "CLOSE";
+        int qos             = 2;
+        String broker       = "tcp://broker.hivemq.com:1883";
+        String clientId     = "SmartBlindsApp";
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(false);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+            System.out.println("Publishing message: "+content);
+            MqttMessage message = new MqttMessage(content.getBytes());
+            message.setQos(qos);
+            sampleClient.publish(topic, message);
+            System.out.println("Message published");
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+}
